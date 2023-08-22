5553bdb73e4166f54f94ce5a7743e440c594a855
Hgarnell <hannah@whiterabbit.com>
2023-08-22 20:22:23 +1200

    backend setup

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -13,7 +13,7 @@ contract Marketplace {
         uint id;
         string name;
         uint price;
-        address owner;
+        address payable owner;
         bool purchased;
     }
 
@@ -21,7 +21,14 @@ contract Marketplace {
         uint id,
         string name,
         uint price,
-        address owner,
+        address payable owner,
+        bool purchased
+    );
+        event ProductPurchased(
+        uint id,
+        string name,
+        uint price,
+        address payable owner,
         bool purchased
     );
 
@@ -49,5 +56,31 @@ contract Marketplace {
         );
         // Trigger an event
         emit ProductCreated(productCount, _name, _price, msg.sender, false);
+    }
+
+    function purchaseProduct(uint _id) public payable {
+        //Fetch the produxct
+        Product memory _product = products[_id];
+        //fetch the owner
+        address payable _seller = _product.owner;
+        //make sure the product is valid
+        require(_product.id > 0 &&_product.id <= productCount);
+        //require that there is enough ether in the transactopm
+        require(msg.value >= _product.price);
+        //require that the product has not been purchased already
+        require(!_product.purchased);
+        //require that buyer is not sender
+        require(_seller != msg.sender);
+
+        //transfer ownership to the buyer
+        _product.owner = msg.sender;
+        //mark as purchaseds
+        _product.purchased = true;
+        //update produt
+        products[_id] = _product;
+        //pay seller by sending them eather
+        address(_seller).transfer(msg.value);
+        //trigger an event
+        emit ProductPurchased(productCount, _product.name , _product.price, msg.sender, true);
     }
 }
--- test/Marketplace.text.js
+++ test/Marketplace.text.js
@@ -50,5 +50,47 @@ contract("Marketplace", ([deployer, seller, buyer]) => {
       //failure: product must have a price
       await await marketplace.createProduct("iPhone X", 0, { from: seller }).should.be.rejected;
     });
+
+    it("Sells Products", async () => {
+                           //track the old seller balance before purchase
+                           let oldSellerBalance;
+                           oldSellerBalance = await web3.eth.getBalance(seller);
+                           oldSellerBalance = new web3.utils.BN(oldSellerBalance);
+
+                           //Success: Buyer makes purchase
+                           result = await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei("1", "Ether") });
+
+                           //check logs
+                           //
+                           const event = result.logs[0].args;
+                           assert.equal(event.id.toNumber(), productCount.toNumber(), "id is correct");
+                           assert.equal(event.name, "Coffee Beans", "name is correct");
+                           assert.equal(event.price, "1000000000000000000", "price is correct");
+                           assert.equal(event.owner, buyer, "owner is correct");
+                           assert.equal(event.purchased, true, "purchased is correct");
+
+                           //chheck seller recieved funds
+                           let newSellerBalance;
+                           newSellerBalance = await web3.eth.getBalance(seller);
+                           newSellerBalance = new web3.utils.BN(newSellerBalance);
+
+                           let price;
+                           price = web3.utils.toWei("1", "Ether");
+                           price = new web3.utils.BN(price);
+
+                           const expectedBalance = oldSellerBalance.add(price);
+
+                           assert.equal(newSellerBalance.toString(), expectedBalance.toString());
+                           //CFailure tries to buy a product trhat does not ezxist
+                           await marketplace.purchaseProduct(99, { from: buyer, value: web3.utils.toWei("1", "Ether") }).should.be.rejected; // FAILURE: Buyer tries to buy without enough ether
+                           //Failure buyer tries to buy wuthout enough ether
+                             await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('0.5', 'Ether') }).should.be.rejected;
+                           //Failure: deployer tries to buy the product
+                             await marketplace.purchaseProduct(productCount, { from: deployer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
+
+                           //Failure BUyer tries to buy again
+                             await marketplace.purchaseProduct(productCount, { from: buyer, value: web3.utils.toWei('1', 'Ether') }).should.be.rejected;
+
+                         });
   });
 });
